d47cccf151a26fca366a4b89e4ab59822644b472
spidergod <spidergod23@gmail.com>
2024-05-14 10:46:09 +0800

    Create index.js

---
+++ src/platforms/demoSystem/store/modules/index.js
@@ -0,0 +1,13 @@
+import app from './app';
+import settings from './settings';
+import tagsView from './tagsView';
+import user from './user';
+import i18n from './i18n';
+
+export {
+  app,
+  settings,
+  tagsView,
+  i18n,
+  user,
+};
