04ec010726f8e239a363b69f825ccd2936c8b339
spidergod <spidergod23@gmail.com>
2024-05-14 10:43:46 +0800

    Create index.vue

---
+++ src/platforms/demoSystem/views/demo/svg-demo/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-row class="flex-wrap" :gutter="20">
+    <el-col v-for="fileName in svgFileList" :key="fileName" class="py-4" :span="4">
+      <div class="d-flex flex-column justify-center">
+        <svg-icon class="mx-auto" :icon-class="fileName" />
+        <span class="pt-3 mx-auto">{{ fileName }}</span>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'SvgDemo',
+  data() {
+    return {
+      svgFileList: [],
+    };
+  },
+  computed: {
+    find() {
+      return 1;
+    },
+  },
+  created() {
+    require.context('@/icons/svg', true, /^.*\.svg$/).keys().forEach((fileName) => {
+      this.svgFileList.push(fileName.replace('./', '').split('.')[0]);
+    });
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
